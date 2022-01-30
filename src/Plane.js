7acb1b62b44f11b6931e1b0b0edf5b0c49c68b5e
meleelin <sdy2615380450@gmail.com>
2022-01-30 21:12:44 +0800

    test

--- src/Plane.js
+++ src/Plane.js
@@ -1,6 +1,6 @@
 function Plane() {
   return (
-    <mesh rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
+    <mesh rotation={[-Math.PI / 2, 0.0, 0]} receiveShadow>
       <planeBufferGeometry attach="geometry" args={[1000, 1000]} />
       <meshPhongMaterial attach="material" color="#cccccc" />
     </mesh>
